f8e7d2ec61f4a0a5e5275805cc7236df0660901a
Sandesh Karki <karkisandesh@hotmail.com>
2020-02-03 17:09:30 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,9 +16,8 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.songs
-    binding.pry
-    artist.name
+    self.songs.artist_id.name
+    
     # return an array of strings containing every musician's name
   end
 end
